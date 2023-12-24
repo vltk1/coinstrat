1923eff49eaee49cd279cfdcc739ceb10cbf32ba
coinstrat <duocifv@gmail.com>
2023-12-24 21:28:51 +0700

    v 0.0.1

---
+++ todo/src/components/index.tsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <h2>121212 </h2>
+}
+export default App;
---
+++ todo/src/index.ts
@@ -0,0 +1,5 @@
+import App from "./components/index.tsx"
+const index = () => {
+  return `1212q   `
+}
+export default index
